e89bdabe7eecabb076b4cc8fd255b9b807aa3e30
yassine riahi <riahiy93@gmail.com>
2024-06-02 21:51:09 +0100

    first-commit

---
+++ android/app/src/main/java/com/project73/MainActivity.java
@@ -0,0 +1,27 @@
+package com.project73;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Project73";
+    }
+
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. We use {@link DefaultReactActivityDelegate}
+     * which allows you to enable New Architecture with a single boolean flag {@link DefaultNewArchitectureEntryPoint#fabricEnabled}.
+     */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(this, getMainComponentName(), DefaultNewArchitectureEntryPoint.getFabricEnabled());
+    }
+}
